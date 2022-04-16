abc8aa910441ec184e8dffc8dbee8904678a7743
gerard <gerard.domenech.cobos@gmail.com>
2022-04-16 03:01:55 +0000

    basic screen with react native

--- web/src/app/pages/Home.tsx
+++ web/src/app/pages/Home.tsx
@@ -1,32 +1,39 @@
-import React, { useRef } from 'react'
-import { View, Text } from 'react-native'
+import React, { useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
 import { Canvas, useFrame } from '@react-three/fiber'
 
 const HomeScreen = () => {
-
-    const Box = () => {
+    function Box(props) {
         // This reference will give us direct access to the mesh
+        const mesh = useRef()
         // Set up state for the hovered and active state
+        const [hovered, setHover] = useState(false)
+        const [active, setActive] = useState(false)
         // Rotate mesh every frame, this is outside of React without overhead
+        useFrame(() => (mesh.current.rotation.x += 0.01))
 
         return (
             <mesh
-                scale={1}
-            >
+                {...props}
+                ref={mesh}
+                scale={active ? 1.5 : 1}
+                onClick={(event) => setActive(!active)}
+                onPointerOver={(event) => setHover(true)}
+                onPointerOut={(event) => setHover(false)}>
                 <boxGeometry args={[1, 2, 3]} />
-                <meshStandardMaterial color={'hotpink'} />
+                <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
             </mesh>
         )
     }
+
     return (
-        <View style={{ flex: 1, height: '100%', width: '100%' }}>
-            <Canvas>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
-                <Box />
-            </Canvas>
-        </View>
-    );
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    )
 }
 
 export default HomeScreen
