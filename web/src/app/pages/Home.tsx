e565748f638a84cec01a45e263e3b67328f884eb
gerard <gerard.domenech.cobos@gmail.com>
2022-04-16 02:24:54 +0000

    Now we have three js working

--- web/src/app/pages/Home.tsx
+++ web/src/app/pages/Home.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { View, Text } from 'react-native'
-import { Canvas } from '@react-three/fiber'
-import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber'
 
-export default function App() {
-  
-  return (
-    <View style={{flex:1, height:'100%', width:'100%'}}>
-        <Canvas>
+const HomeScreen = () => {
 
-        </Canvas>
-    </View>
-  );
+    const Box = () => {
+        // This reference will give us direct access to the mesh
+        const mesh = useRef()
+        // Set up state for the hovered and active state
+        // Rotate mesh every frame, this is outside of React without overhead
+        useFrame(() => (mesh.current.rotation.x += 0.01))
+
+        return (
+            <mesh
+                ref={mesh}
+                scale={1}
+            >
+                <boxGeometry args={[1, 2, 3]} />
+                <meshStandardMaterial color={'hotpink'} />
+            </mesh>
+        )
+    }
+    return (
+        <View style={{ flex: 1 }}>
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+            </Canvas>,ﬂ
+        </View>
+    );
 }
+
+export default HomeScreen
