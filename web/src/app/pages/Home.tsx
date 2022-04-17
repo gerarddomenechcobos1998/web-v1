1dbc4525614f846f4203ae626c3c9b14e9ad757b
gerard <gerard.domenech.cobos@gmail.com>
2022-04-17 15:32:10 +0000

    Adjusted Basic 3DView and guiControls

--- web/src/app/pages/Home.tsx
+++ web/src/app/pages/Home.tsx
@@ -1,21 +1,45 @@
-import React, { useRef, useState, Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import { Canvas, useFrame, extend} from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import React, { useRef, memo, Suspense } from 'react'
+import { Canvas, useFrame, extend } from '@react-three/fiber'
+import { OrbitControls, Environment } from '@react-three/drei'
 import { View } from 'react-native'
 import { useThree } from '@react-three/fiber' // Put inside a component: This hook gives you access to the state model which contains the default renderer, the scene, your camera, and so on
-import { useControls } from "leva";
+import { useControls, button, Leva } from "leva";
 import InfiniteGridHelper from '../components/3D/InfiniteGridHelper'
-import { Color } from 'three'
+import {Color, sRGBEncoding, ACESFilmicToneMapping, PCFSoftShadowMap } from 'three';
 import Axes from '../components/3D/Axes'
-import guiControls from '../../conf/guiControls';
 
-const HomeScreen = () => {
-    
-    const config = useControls(guiControls);
-    
-    const infiniteGridHelper = extend({InfiniteGridHelper}); //The extend function extends React Three Fiber's catalogue of JSX elements
-    function TorusKnot(props) {
+// fog : niebla
+type Props = {
+    children: React.ReactChild
+}
+
+const HomeScreen = (props: Props) => {
+    //leva
+    const levaConfig = {
+        controls: false,
+        lights: true,
+        camera: false,
+        gridHelper: false,
+        axes: false,
+        ambientLight: true,
+        ambientLightIntensity: {
+            value: 0.5,
+            min: 0,
+            max: 1,
+            step: 0.1
+        },
+        environment: {
+            value: 'city',
+            options: ['', 'sunset', 'dawn', 'night', 'warehouse', 'forest', 'apartment', 'studio', 'city', 'park', 'lobby'],
+        },
+        save: button(() => { console.log('Save config') }),
+        "Add Model": button(async () => { console.log('Add model') })
+    }
+    const config = useControls(levaConfig);
+    // grid helper
+    extend({ InfiniteGridHelper }); //The extend function extends React Three Fiber's catalogue of JSX elements
+
+    const TorusKnot = (props) => {
         // This reference will give us direct access to the mesh
         const mesh = useRef()
         return (
@@ -24,34 +48,122 @@ const HomeScreen = () => {
                 ref={mesh}
             >
                 <torusKnotGeometry args={[2, 1, 128, 4]} />
-                <meshNormalMaterial/>
+                <meshNormalMaterial opacity={1} />
             </mesh>
         )
     }
-    const Scene = () => {
+    // Lights
+    const KeyLight = ({ brightness, color }) => {
+        const light = useRef();
+        React.useEffect(() => {
+            if (light && light.current) {
+                light.current.lookAt(0, 0, 0);
+                //const helper = new RectAreaLightHelper( light.current );
+                //light.current.add( helper ); // helper must be added as a child of the light
+            }
+        }, [light]);
         return (
-          <>
-            <ambientLight intensity={0.5} />
-            <pointLight position={[1, 2, 3]} intensity={0.5} />
-            <pointLight position={[1, 2, -3]} intensity={0.5} />
-            <perspectiveCamera far={10} near={1} position={[-2, 2, 3]} onUpdate={(c) => c.lookAt(2, 1, 0)} />
-            <directionalLight color="red" position={[0, 0, 5]} />
-          </>
+            <rectAreaLight
+                ref={light}
+                width={3}
+                height={3}
+                color={color}
+                intensity={brightness}
+                position={[3, 2, 2]}
+            />
         )
-      }
+    }
+
+    const FillLight = ({ brightness, color }) => {
+        const light = useRef();
+        React.useEffect(() => {
+            if (light && light.current) {
+                light.current.lookAt(0, 0, 0);
+                //const helper = new RectAreaLightHelper( light.current );
+                //light.current.add( helper ); // helper must be added as a child of the light
+            }
+        }, [light]);
+        return (
+            <rectAreaLight
+                ref={light}
+                width={3}
+                height={3}
+                intensity={brightness}
+                color={color}
+                position={[-3, 2, 2]}
+            />
+        );
+    }
+
+    const RimLight = ({ brightness, color }) => {
+        const light = useRef();
+        React.useEffect(() => {
+            if (light && light.current) {
+                light.current.lookAt(0, 0, 0);
+                //const helper = new RectAreaLightHelper( light.current );
+                //light.current.add( helper ); // helper must be added as a child of the light
+            }
+        }, [light]);
+        return (
+            <rectAreaLight
+                ref={light}
+                width={2}
+                height={2}
+                intensity={brightness}
+                color={color}
+                position={[0, 4, -4]}
+            />
+        );
+    }
+
+    const Lights = () => {
+        return (
+            <group position={[0, 0, 0]}>
+                <KeyLight brightness={10} color="#fff" />
+                <FillLight brightness={20} color="#fff" />
+                <RimLight brightness={44} color="#fff" />
+            </group>
+        );
+    }
+
+    // const getControls = () => props.controls ?? <OrbitControls target={[0, 0.5, 0]} />;
+    // const getCamera = () => props.camera ?? <PerspectiveCamera position={[10, 6.6, 10]} near={1} far={9999999999} fov={30} /> 
+
     return (
-        <View style={{ flex:1 }}>
-        <Canvas shadows={true} camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 40] }}>
-        <Suspense fallback={null}>
-            <OrbitControls/>
-            <TorusKnot position={[0, 0, 0]} />
-            <Scene />
-            {config.gridHelper && <infiniteGridHelper args={[1, 1 * 10, new Color('#ccc'), 1000]}/>}
-            {config.axes && <Axes/>}
-        </Suspense>
-        </Canvas>
+        <View style={{ flex: 1 }}>
+            <Leva hidden={false} />
+            <Canvas 
+                shadows={true} 
+                camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 40] }}
+                onCreated={({ camera, raycaster, gl }) => {
+                    raycaster.layers.enableAll();
+                    gl.toneMapping = ACESFilmicToneMapping;
+                    gl.toneMappingExposure = 0.9;
+                    gl.outputEncoding = sRGBEncoding;
+                    //gl.physicallyCorrectLights = true;
+                    gl.shadowMap.enabled = true;
+                    gl.shadowMap.type = PCFSoftShadowMap;
+                }}   
+                gl={{antialias: true }}
+                style={{flex:1}}
+            >
+                <fog attach="fog" args={['#ffffff', 0, 100]} />
+                <color attach="background" args={['#d0d0d0']} />
+                <group position={[0, 0, 0]}>
+                    <OrbitControls />
+                    <TorusKnot position={[0, 5, 0]} />
+                    {config.ambientLight && <ambientLight intensity={config.ambientLightIntensity} />}
+                    {config.lights && <Lights />}
+                    {config.gridHelper && <infiniteGridHelper args={[1, 1 * 10, new Color('#ccc'), 1000]} />}
+                    {config.axes && <Axes />}
+                    <Suspense fallback={<></>}>
+                        {config.environment && <Environment preset={'city'} />}
+                        {props.children}
+                    </Suspense>
+                </group>
+            </Canvas>
         </View>
     )
 }
 
-export default HomeScreen
+export default memo(HomeScreen);
